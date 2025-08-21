738519cd4e66d99de8535dd997860d4673aed912
nevis <nevisbalmaceda02@gmail.com>
2025-08-20 22:48:59 -0300

    estos son los cambios de diseño xd

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('menu_dueño', views.menu_dueño, name='menu_dueño'),
+    path('menu_costurero', views.menu_costurero, name='menu_costurero'),
+    path('menu_vendedores', views.menu_vendedores, name='menu_vendedores'),
+    
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 def inicio(request):
     return render(request, "mi_app/inicio.html")
+
+def menu_dueño(request):
+    return render(request, "mi_app/menu_dueño.html")
+
+def menu_costurero(request):
+    return render(request, "mi_app/menu_costurero.html")
+
+def menu_vendedores(request):
+    return render(request, "mi_app/menu_vendedores.html")
+
 # Create your views here.
